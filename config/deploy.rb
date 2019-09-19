7e49af8ff38be39ce1c7c257bba7bc6624ac04c6
pu-chan <48175572+pu-chan@users.noreply.github.com>
2019-09-19 15:05:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,9 @@ lock "~> 3.10.2"
 #
 # あなたのGitHubユーザー名に変更してください
 #
-set :repo_url, "https://github.com/xxxx/20180430-schoo.git"
+set :repo_url, "https://github.com/pu-chan/20180430-schoo.git"
 
 #
 # あなたのWebサーバーの環境に合わせてデプロイ先ディレクトリパスを設定してください
 #
-set :deploy_to, "/var/www/schoo"
+set :deploy_to, "/var/www/html"
